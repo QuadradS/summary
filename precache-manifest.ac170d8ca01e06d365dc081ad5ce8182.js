83990fb585960eb35c4218745c3492ef735e869c
Danil <quadrad4@gmail.com>
2020-08-05 18:24:15 +0600

    Updates

---
+++ precache-manifest.ac170d8ca01e06d365dc081ad5ce8182.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7950d2fa871a2c4cb019ff54ade41df5",
+    "url": "/QuadradS/summary/index.html"
+  },
+  {
+    "revision": "15e4a2df78a0e2022426",
+    "url": "/QuadradS/summary/static/css/main.a38edbef.chunk.css"
+  },
+  {
+    "revision": "8ccf1f23c7a24bb0ecd3",
+    "url": "/QuadradS/summary/static/js/2.63d5d79f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/QuadradS/summary/static/js/2.63d5d79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e4a2df78a0e2022426",
+    "url": "/QuadradS/summary/static/js/main.0a9f1ae2.chunk.js"
+  },
+  {
+    "revision": "779c8aec702efe1e3878",
+    "url": "/QuadradS/summary/static/js/runtime-main.f074b85a.js"
+  },
+  {
+    "revision": "65a1db9b8d44b40ecee4c929ea61ca2e",
+    "url": "/QuadradS/summary/static/media/contacts-icon.65a1db9b.svg"
+  },
+  {
+    "revision": "72a592565c003b8b67733cc326bda35e",
+    "url": "/QuadradS/summary/static/media/mind-icon.72a59256.svg"
+  },
+  {
+    "revision": "bd0955fe99c9ec4a58ecb2f845f7f277",
+    "url": "/QuadradS/summary/static/media/outlined-dot-icon.bd0955fe.svg"
+  },
+  {
+    "revision": "2341860f647e87a840ac059043a71ca9",
+    "url": "/QuadradS/summary/static/media/user-icon.2341860f.svg"
+  },
+  {
+    "revision": "3ed5d265c23d7b44616bd625258eecf8",
+    "url": "/QuadradS/summary/static/media/work-icon.3ed5d265.svg"
+  }
+]);
