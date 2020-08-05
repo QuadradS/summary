9aa9f3715f31a84f3921d7d9d93cf0d7da269581
Danil <quadrad4@gmail.com>
2020-08-05 18:00:03 +0600

    Update build

---
+++ precache-manifest.9540e268989dcc77f0444e29aff01aa4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5561526ce30f86732304df26eac266a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e4a2df78a0e2022426",
+    "url": "/static/css/main.a38edbef.chunk.css"
+  },
+  {
+    "revision": "8ccf1f23c7a24bb0ecd3",
+    "url": "/static/js/2.63d5d79f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63d5d79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e4a2df78a0e2022426",
+    "url": "/static/js/main.0a9f1ae2.chunk.js"
+  },
+  {
+    "revision": "c410b4f4482fdf2cf400",
+    "url": "/static/js/runtime-main.f69e6631.js"
+  },
+  {
+    "revision": "65a1db9b8d44b40ecee4c929ea61ca2e",
+    "url": "/static/media/contacts-icon.65a1db9b.svg"
+  },
+  {
+    "revision": "72a592565c003b8b67733cc326bda35e",
+    "url": "/static/media/mind-icon.72a59256.svg"
+  },
+  {
+    "revision": "bd0955fe99c9ec4a58ecb2f845f7f277",
+    "url": "/static/media/outlined-dot-icon.bd0955fe.svg"
+  },
+  {
+    "revision": "2341860f647e87a840ac059043a71ca9",
+    "url": "/static/media/user-icon.2341860f.svg"
+  },
+  {
+    "revision": "3ed5d265c23d7b44616bd625258eecf8",
+    "url": "/static/media/work-icon.3ed5d265.svg"
+  }
+]);
--- src/theme.js
+++
@@ -1,6 +0,0 @@
-export const theme = {
-  primaryDark: 'rgba(255,255,255,0.96)',
-  primaryLight: '#ffe237',
-  primaryHover: 'rgba(197,250,255,0.14)',
-  mobile: '576px',
-}
